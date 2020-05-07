77b474e1beeaba4977a81e6ad9efbdaee2e25d60
ChrisKnowsDev <chrisharley81@gmail.com>
2020-05-07 17:18:32 -0400

    added google map api functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,102 +7,101 @@ const showcaseImage = document.querySelector('#showcase img');
 const btnSubmit = document.querySelector('#btn-submit');
 const userInput = document.querySelector('#search');
 const submitForm = document.querySelector('#main-form');
-let cardDiv = document.querySelector('.info-cards');
+const cardDiv = document.querySelector('.info-cards');
 
-//handle user submit
-function handleButtonClick(e){
-    e.preventDefault();
-    let searchInputValue = userInput.value; //assigned user input value to searchInputValue
-    // cardInfo(cardInformation);
-    // console.log(searchInputValue); 
-    searchInputValue = searchInputValue.charAt(0).toUpperCase() + searchInputValue.slice(1);
-    //passing the input value ${searchInputValue} to the function eatInfo
-    eatInfo(searchInputValue);
-    hotelInfo(searchInputValue);
-    tourInfo(searchInputValue);
-    nightlifeInfo(searchInputValue);
-    sightSeeingInfo(searchInputValue);
-    // const urlEncoded = encodedURIComponent(searchInputValue);  //urlEncoded will not work due to api search input New_York_City
-    const endpoint = `https://www.triposo.com/api/20200405/location.json?id=${searchInputValue}&fields=all&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
+// handle user submit
+function handleButtonClick(e) {
+  e.preventDefault();
+  let searchInputValue = userInput.value; // assigned user input value to searchInputValue
+  // cardInfo(cardInformation);
+  // console.log(searchInputValue);
+  searchInputValue =
+    searchInputValue.charAt(0).toUpperCase() + searchInputValue.slice(1);
+  // passing the input value ${searchInputValue} to the function eatInfo
+  eatInfo(searchInputValue);
+  hotelInfo(searchInputValue);
+  tourInfo(searchInputValue);
+  nightlifeInfo(searchInputValue);
+  sightSeeingInfo(searchInputValue);
+  // const urlEncoded = encodedURIComponent(searchInputValue);  //urlEncoded will not work due to api search input New_York_City
+  const endpoint = `https://www.triposo.com/api/20200405/location.json?id=${searchInputValue}&fields=all&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
     .then(response => response.json())
     .then(data => {
-        // console.log(searchInputValue); //testing to console log searchinputvalue
-        let userSelectionJSON = JSON.stringify(data); 
-        sessionStorage.setItem('userInformation', userSelectionJSON);//takes the fetched data and save in the session storage
-        window.location.href = "info.html";
-    
-        })
-        .catch(err => console.log(err));
+      // console.log(searchInputValue); //testing to console log searchinputvalue
+      const userSelectionJSON = JSON.stringify(data);
+      sessionStorage.setItem('userInformation', userSelectionJSON); // takes the fetched data and save in the session storage
+      window.location.href = 'info.html';
+    })
+    .catch(err => console.log(err));
 }
 
-function eatInfo (cardInformation){
-    console.log(cardInformation);
-    //top 3 interesting finds 
-    const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=eatingout&count=3&fields=id,name,score,price_tier&order_by=-eatingout_score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
-        .then(response => response.json())
-        .then(data => {
-            let eatInfoJSON = JSON.stringify(data); 
-            sessionStorage.setItem('eatInfo', eatInfoJSON);//takes the poi data and save in the session storage
-        })
-        .catch(err => console.log(err));
+function eatInfo(cardInformation) {
+  console.log(cardInformation);
+  // top 3 interesting finds
+  const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=eatingout&count=3&fields=id,name,score,price_tier&order_by=-eatingout_score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
+    .then(response => response.json())
+    .then(data => {
+      const eatInfoJSON = JSON.stringify(data);
+      sessionStorage.setItem('eatInfo', eatInfoJSON); // takes the poi data and save in the session storage
+    })
+    .catch(err => console.log(err));
 }
 // listens for user submit event,
-btnSubmit.addEventListener('click', handleButtonClick)
+btnSubmit.addEventListener('click', handleButtonClick);
 
-function hotelInfo (cardInformation){
-    console.log(cardInformation);
-    //top 3 hotels  
-    const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=hotels&count=3&fields=id,name,score,price_tier&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
+function hotelInfo(cardInformation) {
+  // top 3 hotels
+  const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=hotels&count=3&fields=id,name,score,price_tier&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
     .then(response => response.json())
     .then(data => {
-            let hotelInfoJSON = JSON.stringify(data); 
-            sessionStorage.setItem('hotelInfo', hotelInfoJSON);//takes the poi data and save in the session storage
-        })
-        .catch(err => console.log(err));
+      const hotelInfoJSON = JSON.stringify(data);
+      sessionStorage.setItem('hotelInfo', hotelInfoJSON); // takes the poi data and save in the session storage
+    })
+    .catch(err => console.log(err));
 }
 
-function tourInfo (cardInformation){
-    //top 3 tours 
-    const endpoint = `https://www.triposo.com/api/20200405/tour.json?location_ids=${cardInformation}&count=3&fields=id,name,score,price,price_is_per_person,vendor,intro,tag_labels&order_by=-score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
+function tourInfo(cardInformation) {
+  // top 3 tours
+  const endpoint = `https://www.triposo.com/api/20200405/tour.json?location_ids=${cardInformation}&count=3&fields=id,name,score,price,price_is_per_person,vendor,intro,tag_labels&order_by=-score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
     .then(response => response.json())
     .then(data => {
-            let tourInfoJSON = JSON.stringify(data); 
-            sessionStorage.setItem('tourInfo', tourInfoJSON);//takes the poi data and save in the session storage
-        })
-        .catch(err => console.log(err));
+      const tourInfoJSON = JSON.stringify(data);
+      sessionStorage.setItem('tourInfo', tourInfoJSON); // takes the poi data and save in the session storage
+    })
+    .catch(err => console.log(err));
 }
 
-function nightlifeInfo (cardInformation){
-    //top 3 night life 
-    const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=nightlife&count=3&fields=id,name,score,price_tier&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
+function nightlifeInfo(cardInformation) {
+  // top 3 night life
+  const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=nightlife&count=3&fields=id,name,score,price_tier&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
     .then(response => response.json())
     .then(data => {
-            let nightlifeInfoJSON = JSON.stringify(data); 
-            sessionStorage.setItem('nightLifeInfo', nightlifeInfoJSON);//takes the poi data and save in the session storage
-        })
-        .catch(err => console.log(err));
+      const nightlifeInfoJSON = JSON.stringify(data);
+      sessionStorage.setItem('nightLifeInfo', nightlifeInfoJSON); // takes the poi data and save in the session storage
+    })
+    .catch(err => console.log(err));
 }
 
-function sightSeeingInfo (cardInformation){
-    //top 3 sightSeeing information 
-    const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=sightseeing&bookable=1&count=3&fields=id,name,score,intro,booking_info&order_by=-score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
-    const searchPromise = fetch(endpoint);
-    searchPromise
+function sightSeeingInfo(cardInformation) {
+  // top 3 sightSeeing information
+  const endpoint = `https://www.triposo.com/api/20200405/poi.json?location_id=${cardInformation}&tag_labels=sightseeing&bookable=1&count=3&fields=id,name,score,intro,booking_info&order_by=-score&account=3XPHWAVV&token=ubjfil8myjnlk6z1t6m3dehs96y9upct`;
+  const searchPromise = fetch(endpoint);
+  searchPromise
     .then(response => response.json())
     .then(data => {
-        console.log(cardInformation);
-            let sightSeeingInfoJSON = JSON.stringify(data); 
-            sessionStorage.setItem('sightSeeingInfo', sightSeeingInfoJSON);//takes the poi data and save in the session storage
-        })
-        .catch(err => console.log(err));
+      console.log(cardInformation);
+      const sightSeeingInfoJSON = JSON.stringify(data);
+      sessionStorage.setItem('sightSeeingInfo', sightSeeingInfoJSON); // takes the poi data and save in the session storage
+    })
+    .catch(err => console.log(err));
 }
--- dist/js/info.js
+++ dist/js/info.js
@@ -1,7 +1,3 @@
-const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapApiKey}&callback=initMap`; 
-document.body.appendChild(script);
-
 const userInformation = sessionStorage.getItem('userInformation');
 const travelInfo = JSON.parse(userInformation);
 const eatInfoPassJSON = sessionStorage.getItem('eatInfo');
@@ -15,36 +11,34 @@ const nightlifeInfoParse = JSON.parse(nightlifeInfoPassJSON);
 const sightSeeingInfoPassJSON = sessionStorage.getItem('sightSeeingInfo');
 const sightSeeingInfoParse = JSON.parse(sightSeeingInfoPassJSON);
 
-
 function locationStatus(travelInfo) {
-    const data = travelInfo.results[0];
-    statsCity.innerHTML = data.id
-    statsCountry.innerHTML = data.country_id;
-    statsScore.innerHTML = data.score.toFixed(1);
-    statsAbout.innerHTML = data.intro;
-    showcaseImage.setAttribute('src', data.images[0].source_url)
-
-};
+  const data = travelInfo.results[0];
+  statsCity.innerHTML = data.id;
+  statsCountry.innerHTML = data.country_id;
+  statsScore.innerHTML = data.score.toFixed(1);
+  statsAbout.innerHTML = data.intro;
+  showcaseImage.setAttribute('src', data.images[0].source_url);
+}
 locationStatus(travelInfo);
 
-
-function createCards(eatInfo, title) { 
-    // console.log("ssssssssss");
-    console.log(title)    
-    let renderCards = eatInfo.results.map((card) => {
-        return `
+function createCards(eatInfo, title) {
+  // console.log("ssssssssss");
+  const renderCards = eatInfo.results.map(
+    card => `
         <div class="card-info">
         <ul class="info-1">
         <li>Name: <span class="name-span1">${card.name}</span></li>
         <li>Price Tier: <span class="price-span1">${card.price_tier}</span></li>
-        <li>Score: <span class="score-span1">${card.score.toFixed(1)}</span></li>
+        <li>Score: <span class="score-span1">${card.score.toFixed(
+          1
+        )}</span></li>
         </ul>
         </div>
         `
-    });
-    // console.log(eatInfo);
-    // building the cards here 
-    return `
+  );
+  // console.log(eatInfo);
+  // building the cards here
+  return `
     <div class="card">
     <h3>${title}</h3>
     ${renderCards.join('')}
@@ -53,29 +47,27 @@ function createCards(eatInfo, title) {
 }
 const infoCards = document.querySelector('.info-cards');
 const title = 'Top3: Restaurants';
-const titleHotel = 'Top3: Hotels'; 
+const titleHotel = 'Top3: Hotels';
 const titleTour = 'Top3: Tours';
 const titleNightlife = 'Top3: Nightlife';
-const titleSightSeeingInfo = 'Top3: Sightseeing'
-//add variables here........................................
+const titleSightSeeingInfo = 'Top3: Sightseeing';
+// add variables here........................................
 infoCards.innerHTML = createCards(eatInfoParse, title);
 infoCards.innerHTML += createCards(hotelInfoParse, titleHotel);
 infoCards.innerHTML += createCards(tourInfoParse, titleTour);
 infoCards.innerHTML += createCards(nightlifeInfoParse, titleNightlife);
 infoCards.innerHTML += createCards(sightSeeingInfoParse, titleSightSeeingInfo);
 
-//google maps
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-
-
-
-
+const lat = travelInfo.results[0].coordinates.latitude;
+const lng = travelInfo.results[0].coordinates.longitude;
+// google maps
+let map;
+function initMap(data) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat, lng },
+    zoom: 8,
+  });
+}
 
 // Create the script tag, set the appropriate attributes
 // var script = document.createElement('script');
@@ -90,4 +82,3 @@ var map;
 
 // // Append the 'script' element to 'head'
 // document.head.appendChild(script);
-      
