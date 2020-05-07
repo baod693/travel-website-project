4df7aea9c99f32f6ac7d5ebb1b162e98946537e5
hudaman0621 <mrhu828@gmail.com>
2020-05-07 16:20:21 -0400

    google map added

--- dist/js/info.js
+++ dist/js/info.js
@@ -1,3 +1,7 @@
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapApiKey}&callback=initMap`; 
+document.body.appendChild(script);
+
 const userInformation = sessionStorage.getItem('userInformation');
 const travelInfo = JSON.parse(userInformation);
 const eatInfoPassJSON = sessionStorage.getItem('eatInfo');
@@ -60,7 +64,30 @@ infoCards.innerHTML += createCards(tourInfoParse, titleTour);
 infoCards.innerHTML += createCards(nightlifeInfoParse, titleNightlife);
 infoCards.innerHTML += createCards(sightSeeingInfoParse, titleSightSeeingInfo);
 
+//google maps
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
 
 
 
+
+// Create the script tag, set the appropriate attributes
+// var script = document.createElement('script');
+// script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap`;
+// script.defer = true;
+// script.async = true;
+
+// // Attach your callback function to the `window` object
+// window.initMap = function() {
+//   // JS API is loaded and available
+// };
+
+// // Append the 'script' element to 'head'
+// document.head.appendChild(script);
+      
