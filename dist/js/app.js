6847de264670f74e02c8f74ed1267f865b75a79a
ChrisKnowsDev <chrisharley81@gmail.com>
2020-05-10 13:22:06 -0400

    changed alert text

--- dist/js/app.js
+++ dist/js/app.js
@@ -25,8 +25,6 @@ function handleButtonClick(e) {
     const splitStr = str.toLowerCase().split('_');
 
     for (let i = 0; i < splitStr.length; i += 1) {
-      // You do not need to check if i is larger than splitStr length, as your for does that for you
-      // Assign it back to the array
       splitStr[i] =
         splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
